cc14e7e5fbac108a98b81d0cf07ba3759c4b3672
VatsalyaGoswami <vatsalya559@gmail.com>
2021-08-21 00:46:34 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <div className='Footer'>
+            <p className="Footer-text">Vatsalya © 2021</p>
+        </div>
+    )
+}
+
+export default Footer
+
+
